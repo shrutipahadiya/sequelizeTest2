4086a84eb6c6fb79f215b23f50e16c0691dc865b
Shruti Pahadiya <15.shruti@gmail.com>
2020-06-29 18:57:08 -0400

    sequelize stretch 2 not complete

--- pumpkin.js
+++ pumpkin.js
@@ -3,5 +3,32 @@ const Sequelize = require('sequelize');
 
 //YOUR CODE GOES HERE
 
+const Pumpkin = db.define('Pumpkin',{
+        name:{
+            type:Sequelize.STRING
+        },
+        size:{
+            type:Sequelize.ENUM('small','medium','large')
+        },
+        evil:{
+            type:Sequelize.BOOLEAN,
+            defaultValue : false
+        },
+        carved:{
+            type:Sequelize.BOOLEAN
+        },
+        candle:{
+            type:Sequelize.BOOLEAN
+        }
+
+
+});
+
+
+Pumpkin.prototype.lightcandle = async() => {
+   return await 
+}
+
+
 //--------------------
 module.exports = Pumpkin;
